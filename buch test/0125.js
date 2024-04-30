153178977d52ab1301a1ee215d271a0183a38c54
simonkuhlo <siga241@gmail.com>
2024-04-30 09:05:00 +0200

    finish js

--- buch test/0125.js
+++ buch test/0125.js
@@ -1,58 +1,58 @@
-let pages = document.getElementsByClassName("f0122-page");
+let pages;
+let last_action;
 let current_page = 0;
 const max_pages = 99;
 
+window.onload = function setup() {
+  pages = document.getElementsByClassName("f0122-page");
+  for (i=0; i < pages.length-1; i++) {
+    let page = pages[i];
+    if (page.classList.contains("f0122-flipped")) {
+      page.classList.remove("f0122-flipped");
+    }
+    page.style.zIndex = 0 - i;
+  }
+}
 
 function next_page() {
-  flip_page_2();
+  flip_page();
 }
 
 function prev_page() {
-  unflip_page();
+  flip_page(unflip=true);
 }
 
-function flip_page_2() {
-    if (current_page < pages.length-1) {
-        let page = pages[current_page];
-        page.classList.add("f0122-flipped");
-        current_page++;
-    }
-    console.log(current_page)
-}
-function unflip_page() {
+function flip_page(unflip=false) {
+  let page;
+  if (unflip == true) {
     if (current_page >= 1) {
-        let page = pages[current_page-1];
-        page.classList.remove("f0122-flipped");
-        current_page--;
+      page = pages[current_page-1];
+      page.classList.remove("f0122-flipped");
+      current_page--;
+      last_action = "unflip";
     }
-    console.log(current_page)
+    
+  } else {
+    if (current_page < pages.length-1) {
+      page = pages[current_page];
+      page.classList.add("f0122-flipped");
+      current_page++;
+      last_action = "flip";
+    }
+  }
+  setTimeout(set_zindex, 1000);
 }
 
-function flip_page(page_number, animation) {
-  let page_to_flip = document.getElementById(`f012-page${page_number}`);
-  let computedStyle = window.getComputedStyle(page_to_flip);
-  let animation_duration = computedStyle.animationDuration;
-  let z_index = computedStyle.zIndex;
-  z_index = parseInt(z_index);
-  let animation_duration_ms = parseFloat(animation_duration) * 1000;
-  let halfway_point = animation_duration_ms / 2;
-  page_to_flip.style.animationName = animation;
-  setTimeout(() => {
-    page_to_flip.style.zIndex = 0 - z_index;
-  }, halfway_point);
-  
-} 
-
-function move_book(next_page) {
-  if (next_page < 2){
-    book = document.getElementById(`book`);
-    let computedStyle_book = window.getComputedStyle(book);
-    let width = parseFloat(computedStyle_book.width);
-    let move_value = width / 2;
-    if(current_page == 0 && next_page == 1){
-      book.style.transform = `translateX(${move_value}px)`;
-    } else if (current_page == 1 && next_page == 0){
-      book.style.transform = `translateX(-${move_value}px)`;
-    }
-  } 
+function set_zindex() {
+  let page;
+  if (last_action == "unflip") {
+    page = pages[current_page];
+  } else {
+    page = pages[current_page-1];
+  }
+  if (page.classList.contains("f0122-flipped")) {
+    page.style.zIndex = current_page - 1;
+  } else {
+    page.style.zIndex = 0 - current_page;
+  }
 }
